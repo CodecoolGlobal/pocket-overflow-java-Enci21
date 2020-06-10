b27d39ae12900aa2f8154dc64ef9fcc26c549a3b
Enci21 <eni.hegedus.95@gmail.com>
2020-06-10 15:38:30 +0200

    Main menu activity

---
+++ PocketOverflow/app/src/main/java/com/example/pocketoverflow/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.pocketoverflow;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pocketoverflow.registration.RegistrationActivity;
+import com.example.pocketoverflow.signIn.SignInActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.everyoneElseButton)
+    Button everyoneElseButton;
+
+    @BindView(R.id.firstYearsButton)
+    Button firstYearsButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        everyoneElseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        firstYearsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
