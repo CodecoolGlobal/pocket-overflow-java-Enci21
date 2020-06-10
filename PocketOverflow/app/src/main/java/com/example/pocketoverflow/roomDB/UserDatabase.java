1abd693abbcdf1ac4a9c565c4b3162357e9f9c71
Enci21 <eni.hegedus.95@gmail.com>
2020-06-10 15:37:14 +0200

    Create Room database

---
+++ PocketOverflow/app/src/main/java/com/example/pocketoverflow/roomDB/UserDatabase.java
@@ -0,0 +1,31 @@
+package com.example.pocketoverflow.roomDB;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {User.class}, version = 1, exportSchema = false)
+public abstract class UserDatabase extends RoomDatabase {
+
+    private static final int NUM_OF_THREADS = 4;
+    static final ExecutorService databaseWithExecutor = Executors.newFixedThreadPool(NUM_OF_THREADS);
+    private static volatile UserDatabase db;
+
+    static UserDatabase getDb(final Context context) {
+        if (db == null) {
+            synchronized (UserDatabase.class) {
+                db = Room.databaseBuilder(context.getApplicationContext(),
+                        UserDatabase.class, "db")
+                        .build();
+            }
+        }
+        return db;
+    }
+
+    public abstract UserDao dao();
+}
