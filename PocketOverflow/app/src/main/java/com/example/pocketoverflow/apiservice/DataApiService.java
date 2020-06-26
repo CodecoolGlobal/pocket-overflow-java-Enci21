5374d27c89c3cd110f4128920cb4c1feda68472a
Enci21 <eni.hegedus.95@gmail.com>
2020-06-26 13:03:03 +0200

    Create api service

--- PocketOverflow/app/src/main/java/com/example/pocketoverflow/apiservice/DataApiService.java
+++ PocketOverflow/app/src/main/java/com/example/pocketoverflow/apiservice/DataApiService.java
@@ -1,4 +1,4 @@
-package com.example.pocketoverflow.signIn.ui;
+package com.example.pocketoverflow.apiservice;
 
 
 import com.example.pocketoverflow.signIn.ui.commonRoom.House;
@@ -6,16 +6,16 @@ import com.example.pocketoverflow.signIn.ui.spells.Spell;
 
 import java.util.List;
 
-import io.reactivex.Observable;
+import io.reactivex.Single;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
-public interface JsonPlaceHolderApi {
+public interface DataApiService {
 
     @GET("v1/houses/{houseId}")
-    Observable<List<House>> getHouseById(@Path("houseId") String houseId, @Query("key") String apiKey);
+    Single<List<House>> getHouseById(@Path("houseId") String houseId, @Query("key") String apiKey);
 
     @GET("v1/spells")
-    Observable<List<Spell>> getSpells(@Query("key") String apiKey);
+    Single<List<Spell>> getSpells(@Query("key") String apiKey);
 }
---
+++ PocketOverflow/app/src/main/java/com/example/pocketoverflow/apiservice/RetrofitClient.java
@@ -0,0 +1,29 @@
+package com.example.pocketoverflow.apiservice;
+
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit retrofit = null;
+    private static String BASE_URL = "https://www.potterapi.com/";
+
+    public static Retrofit getClient() {
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(client)
+                    .build();
+        }
+
+        return retrofit;
+    }
+}
